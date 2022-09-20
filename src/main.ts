e0391aca1c9fc46f730e3d400b30489d71736940
Yavuzhan Aymak <yavuzhanaymak@gmail.com>
2022-09-20 03:24:51 +0300

    port 3333

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 3010);
+  await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
